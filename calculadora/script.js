10d310510975908409325222c18e6c93bce5aee0
Gustavo Matheus <117738201+gustanitro@users.noreply.github.com>
2023-09-24 22:13:58 -0300

    fiz as operações

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,11 +1,25 @@
-let displayValue = '';
+let displayValue = ''; //valor do display é nulo
 
 function mostra(value) {
-    displayValue += value;
+    // Atribue à variavel o numero clicado 
+    displayValue += value; 
+   
+    // Mostra o valor da variavel no visor
     document.getElementById('display').value = displayValue;
 }
 
 function LimpaDisplay() {
-    displayValue = '';
+    displayValue = ''; 
     document.getElementById('display').value = displayValue;
 }
+
+function calcular() {
+    // Recupere a expressão do visor
+    const expressao = document.getElementById('display').value;
+        
+    // Avalie a expressão usando a função eval()
+    const result = eval(expressao);
+        
+    // Exiba o resultado no visor
+    document.getElementById('display').value = result;
+    }
